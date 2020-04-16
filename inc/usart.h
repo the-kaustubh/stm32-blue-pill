da1ce575f1433a47f530db5aaf2f4f32e4925012
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2020-04-16 19:38:02 +0530

    revise usart

--- inc/usart.h
+++ inc/usart.h
@@ -3,4 +3,16 @@
 
 #define USART_BASE (0x40013800)
 
+struct usart {
+  unsigned long SR;       /* Address offset: 0x00 */
+	unsigned long DR;       /* Address offset: 0x04 */
+	unsigned long BRR;      /* Address offset: 0x08 */
+	unsigned long CR1;      /* Address offset: 0x0C */
+	unsigned long CR2;      /* Address offset: 0x10 */
+	unsigned long CR3;      /* Address offset: 0x14 */
+	unsigned long GTPR;     /* Address offset: 0x18 */
+};
+
+struct usart *USART = (struct usart *)USART_BASE;
+
 #endif
